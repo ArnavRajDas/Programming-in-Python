07a4c539581254711bc268f09d757ba5627c6b76
ArnavRajDas <arnavrajdas@gmail.com>
2026-09-05 16:03:34 +0530

    02_Python_Fundamentals: feat: solve practice problem 03

--- 01_Learning/02_Python_Fundamentals/Practice/practice_problem_03.py
+++ 01_Learning/02_Python_Fundamentals/Practice/practice_problem_03.py
@@ -11,3 +11,15 @@
  '''
 
 # Solution:
+
+name = "Arnav"
+age = 20
+jee_percentile = 83.66
+is_student = True
+
+print("The name of student is " + name + ", he is " + str(age) + " year old" + ", and his JEE percentile is " + str(jee_percentile) + " and he is a student: " + str(is_student))
+
+
+# or using f-string
+
+print(f"The name of student is {name}, he is {age} year old, and his JEE percentile is {jee_percentile} and He is a student: {is_student}")
