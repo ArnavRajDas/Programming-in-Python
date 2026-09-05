783f716a32c24c69a80ab482c2cd1e4c77f74b55
ArnavRajDas <arnavrajdas@gmail.com>
2026-09-06 00:15:07 +0530

    02_Python_Fundamentals: feat: solve practice problem 07

--- 01_Learning/02_Python_Fundamentals/Practice/practice_problem_07.py
+++ 01_Learning/02_Python_Fundamentals/Practice/practice_problem_07.py
@@ -3,3 +3,7 @@ Print the following output using escape sequences:
 Arnav said, "Python is awesome!"
 This is on a new line.
 This is a tab →	    after tab.'''
+
+# Solution:
+
+print("Arnav said, \"Python is awesome!\"\nThis is on a new line.\nThis is a tab →\tafter tab.")
