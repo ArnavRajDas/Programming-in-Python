64435b176b786b3eec76c913317dc823e952df5a
ArnavRajDas <arnavrajdas@gmail.com>
2026-09-05 14:18:14 +0530

    02_Python_Fundamentals: feat: solve practice problem 02

--- 01_Learning/02_Python_Fundamentals/Practice/practice_problem_02.py
+++ 01_Learning/02_Python_Fundamentals/Practice/practice_problem_02.py
@@ -1,5 +1,6 @@
 ''' Q2: Print a Poem
- Write a program that prints
+ 
+  Write a program that prints
    the following poem using a single print() statement:
 
  (Hint: Use \n for a new line.)
@@ -10,3 +11,11 @@
  '''
 
 # Solution
+
+print("Twinkle, twinkle, little star, \nHow I wonder what you are!")
+
+# or 
+# we can also use multi-line string to print the poem.  
+
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!''')
