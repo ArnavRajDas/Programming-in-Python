da9c81eeb5174f1685c2ddbd65acc450eb2fac3e
ArnavRajDas <arnavrajdas@gmail.com>
2026-09-07 19:09:01 +0530

    02_Python_Fundamentals: feat: solve practice problem 08

--- 01_Learning/02_Python_Fundamentals/Practice/practice_problem_08.py
+++ 01_Learning/02_Python_Fundamentals/Practice/practice_problem_08.py
@@ -3,3 +3,10 @@ Write a program that:
 
 Takes an integer as input from the user.
 Prints the square and cube of that number.'''
+
+# Solution:
+
+a = int(input("Choose any number : "))
+
+print("Squar of", a, "is :", a**2)
+print("Cube of", a, "is :", a**3)
