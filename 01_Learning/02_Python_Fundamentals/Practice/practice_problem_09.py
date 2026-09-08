8968907cd79d9613aca9be15a6dab67fbf7a2047
ArnavRajDas <arnavrajdas@gmail.com>
2026-09-08 19:52:52 +0530

    02_Python_Fundamentals: feat: add15 more problems and solve practice problem 09

--- 01_Learning/02_Python_Fundamentals/Practice/practice_problem_09.py
+++ 01_Learning/02_Python_Fundamentals/Practice/practice_problem_09.py
@@ -3,30 +3,92 @@ Q9: Quick Quiz (True/False)
 Mark each as True or False:
 
 1. Python code must always end with a semicolon ;
--->
+--> False
 
 
 2. The # symbol is used for comments in Python
--->
+--> True
 
 
 3. "123" and 123 are the same in Python
--->
+--> False
 
 
 4. The * operator is used for multiplication
--->
+--> True
 
 
 5. \n creates a new line
--->
+--> True
 
 
 6. Variables in Python can start with numbers
--->
+--> False
 
 
 7. int("10") + 5 gives 15
--->
+--> True
+
+
+8. Python variable names are case-sensitive.
+--> True
+
+
+9. A variable name can contain letters, numbers, and underscores,
+   but it cannot start with a number.
+--> True
+
+
+10. The type of input() is always str unless we explicitly convert it.
+--> True
+
+
+11. 10 / 2 returns an integer in Python.
+--> False
+
+
+12. 10 // 3 returns 3.
+--> True
+
+
+13. The == operator is used to compare values.
+--> True
+
+
+14. The = operator is used to compare two values.
+--> False
+
+
+15. The and operator returns True only when both conditions are True.
+--> True
+
+
+16. The is operator should normally be used to compare whether two values are equal.
+--> False
+
+
+17. The in operator can be used to check whether an item exists inside a list.
+--> True
+
+
+18. The ^ operator is used for exponentiation in Python.
+--> False
+
+
+19. The ~ operator performs a bitwise NOT operation.
+--> True
+
+
+20. Parentheses can be used to change the normal order of operator evaluation.
+--> True
+
+
+21. In the expression 10 + 5 * 2, addition is performed before multiplication.
+--> False
+
+
+22. In Python, 2 ** 3 ** 2 is evaluated from left to right.
+--> False
+
 
 '''
