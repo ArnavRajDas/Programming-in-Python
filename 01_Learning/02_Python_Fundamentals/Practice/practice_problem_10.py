88fbb829fdfca155addbd8c731c1360c95c8d953
ArnavRajDas <arnavrajdas@gmail.com>
2026-09-09 19:56:54 +0530

    02_Python_Fundamentals: chore: add Practice problem 10

---
+++ 01_Learning/02_Python_Fundamentals/Practice/practice_problem_10.py
@@ -0,0 +1,19 @@
+'''Q10: Debugging Practice
+
+The following program contains errors.
+
+Fix the errors so that the program:
+    - Takes two numbers from the user
+    - Calculates their sum
+    - Prints the result
+
+Broken Code:
+
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+print("Sum:", num1 + num2)
+'''
+
+# Soultion:
+
