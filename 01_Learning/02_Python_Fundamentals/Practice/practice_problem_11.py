77ba630a79c88a4688946ab734304a09af3ac569
ArnavRajDas <arnavrajdas@gmail.com>
2026-09-09 20:13:37 +0530

    02_Python_Fundamentals: chore: add Practice problem 11

---
+++ 01_Learning/02_Python_Fundamentals/Practice/practice_problem_11.py
@@ -0,0 +1,24 @@
+'''
+Q11: Explain the Concept
+
+
+1. What is the difference between = and == ?
+--> 
+
+
+2. Why does input() normally return a string?
+-->
+
+
+3. What is the difference between is and == ?
+-->
+
+
+4. Why does 10 + 5 * 2 give 20 instead of 30?
+-->
+
+
+5. What is the difference between / and // ?
+-->
+
+'''
