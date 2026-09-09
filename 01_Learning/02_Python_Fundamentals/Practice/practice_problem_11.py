a6f63d22dcaf77ee86229adf7a7245d1592dbf09
ArnavRajDas <arnavrajdas@gmail.com>
2026-09-09 20:18:31 +0530

    02_Python_Fundamentals: feat: solve practice problem 11

--- 01_Learning/02_Python_Fundamentals/Practice/practice_problem_11.py
+++ 01_Learning/02_Python_Fundamentals/Practice/practice_problem_11.py
@@ -3,22 +3,29 @@ Q11: Explain the Concept
 
 
 1. What is the difference between = and == ?
---> 
+--> = is used to assign a value to a variable,
+   while == is used to compare two values.
 
 
 2. Why does input() normally return a string?
--->
+--> input() returns user input as a string.
+   If we need a number, we have to convert it using
+   int() or float().
 
 
 3. What is the difference between is and == ?
--->
+--> == compares values, while is checks whether two
+   references point to the same object.
 
 
 4. Why does 10 + 5 * 2 give 20 instead of 30?
--->
+--> * has higher precedence than +.
+   Therefore, 5 * 2 is calculated first.
 
 
 5. What is the difference between / and // ?
--->
+--> / performs normal division and returns a float,
+   while // performs floor division.
+   // returns the largest integer less than or equal to the division result.
 
 '''
